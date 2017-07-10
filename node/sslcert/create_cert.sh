24606424732281aa6330da07efb12ba339e4ba75
Thushan Gunawardana <thushan.gunawardana@qmatic.com>
2017-07-10 14:35:13 +0530

    141396885 : Extend validity period for default certificate

--- node/sslcert/create_cert.sh
+++ node/sslcert/create_cert.sh
@@ -5,7 +5,7 @@ openssl req -new -key server.enc.key -out server.csr
 echo "Created cert request using the private key..."
 openssl rsa -in server.enc.key -out server.key
 echo "Created public key 'server.key' using the private key..."
-openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt
 echo "Created dev certificate"
 echo "***************************************************************"
 echo "  NOTE : Please make sure you enable ssl in proxy-config.json"
